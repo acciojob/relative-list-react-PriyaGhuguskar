9ec0f1ebd8cb7bf80604ca0fe3978c48883e01f8
priyaghuguskar <ghuguskarpriya@gmail.com>
2023-01-23 11:06:57 -0800

    logic added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const App = () => {
+  const [relative, setrelative] =useState([relativeListItem1 ,relativeListItem2 ,relativeListItem3,relativeListItem4])
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <ol>
+      {relative.map((rel ,ind)=>{
+        return 
+        <li key={rel + ind}>Name : {rel}</li>
+      })}
+      </ol>
     </div>
   )
 }
